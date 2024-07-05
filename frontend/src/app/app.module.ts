77e871c08c0c8921de0b21e9ec1c951934fff435
sudarshan raveendranath dayarathna <120754357+sudarshan-raveendranath@users.noreply.github.com>
2024-07-05 12:59:50 +0530

    modified:   frontend/src/app/app-routing.module.ts 	modified:   frontend/src/app/app.component.html 	modified:   frontend/src/app/app.module.ts 	new file:   frontend/src/app/employee.service.spec.ts 	new file:   frontend/src/app/employee.service.ts 	new file:   frontend/src/app/employee/employee.component.css 	new file:   frontend/src/app/employee/employee.component.html 	new file:   frontend/src/app/employee/employee.component.spec.ts 	new file:   frontend/src/app/employee/employee.component.ts 	new file:   frontend/src/app/header/header.component.css 	new file:   frontend/src/app/header/header.component.html 	new file:   frontend/src/app/header/header.component.spec.ts 	new file:   frontend/src/app/header/header.component.ts 	new file:   frontend/src/app/home/home.component.css 	new file:   frontend/src/app/home/home.component.html 	new file:   frontend/src/app/home/home.component.spec.ts 	new file:   frontend/src/app/home/home.component.ts 	new file:   frontend/src/assets/images/coming soon.png 	new file:   frontend/src/assets/images/simple crud operations.png

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,14 +4,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatGridListModule
   ],
   providers: [
     provideClientHydration(),
