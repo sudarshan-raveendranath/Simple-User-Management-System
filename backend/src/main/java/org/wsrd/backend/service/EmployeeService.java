5593815b133c226c10c2095bae50179ff94cd2df
sudarshan raveendranath dayarathna <120754357+sudarshan-raveendranath@users.noreply.github.com>
2024-07-05 11:55:06 +0530

    modified:   backend/src/main/java/org/wsrd/backend/controller/EmployeeController.java 	new file:   backend/src/main/java/org/wsrd/backend/dto/EmployeeDto.java 	modified:   backend/src/main/java/org/wsrd/backend/entity/Employee.java 	modified:   backend/src/main/java/org/wsrd/backend/service/EmployeeService.java 	new file:   frontend/.editorconfig 	new file:   frontend/.gitignore 	new file:   frontend/.vscode/extensions.json 	new file:   frontend/.vscode/launch.json 	new file:   frontend/.vscode/tasks.json 	new file:   frontend/README.md 	new file:   frontend/angular.json 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/server.ts 	new file:   frontend/src/app/app-routing.module.ts 	new file:   frontend/src/app/app.component.css 	new file:   frontend/src/app/app.component.html 	new file:   frontend/src/app/app.component.spec.ts 	new file:   frontend/src/app/app.component.ts 	new file:   frontend/src/app/app.module.server.ts 	new file:   frontend/src/app/app.module.ts 	new file:   frontend/src/assets/.gitkeep 	new file:   frontend/src/favicon.ico 	new file:   frontend/src/index.html 	new file:   frontend/src/main.server.ts 	new file:   frontend/src/main.ts 	new file:   frontend/src/styles.css 	new file:   frontend/tsconfig.app.json 	new file:   frontend/tsconfig.json 	new file:   frontend/tsconfig.spec.json

--- backend/src/main/java/org/wsrd/backend/service/EmployeeService.java
+++ backend/src/main/java/org/wsrd/backend/service/EmployeeService.java
@@ -1,7 +1,38 @@
 package org.wsrd.backend.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.wsrd.backend.entity.Employee;
+import org.wsrd.backend.repository.EmployeeRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return new ArrayList<>(employeeRepository.findAll());
+    }
+
+    public Employee getEmployee(Integer id) {
+        return employeeRepository.findById(id).orElseThrow();
+    }
+
+    public String deleteEmployee(Integer id) {
+        employeeRepository.deleteById(id);
+        return "Employee deleted successfully";
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        employeeRepository.findById(employee.getEmpId()).orElseThrow();
+        return employeeRepository.save(employee);
+    }
 }
